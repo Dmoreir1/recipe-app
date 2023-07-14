addc07ee9b08e1f340bfc014dc0d751ca6bf79eb
dmoreir1 <dmoreir1@binghamton.edu>
2023-07-13 22:53:01 -0400

    making updates to endpoints

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+import cors from 'cors'; 
+import mongoose from 'mongoose';
+import express from 'express';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { userRouter } from "./routes/users.js"
+
+const app = express (); 
+
+app.use(express.json()); 
+app.use(cors()); 
+
+app.use("/auth", userRouter);
+
+mongoose.connect("mongodb+srv://dmoreira:REiXLg9RYahiMlyL@cluster3.vxq7p7m.mongodb.net/?retryWrites=true&w=majority");
+
+app.listen(8080, () => console.log("Server Started!"));  
+
+
+
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+  
+//   {/* </React.StrictMode> */}
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
